f6697b520dd323ec365500adbf208cd2da0c5ad7
lucasmarques95 <lucas.marques95@hotmail.com>
2025-02-13 19:10:15 -0300

    Adicionando projeto replica do Spotiy frontend

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ItemList from './ItemList'
+
+const Main = () => {
+    return (
+        <div className="main">
+            <ItemList />
+
+            <ItemList />
+        </div>
+    )
+}
+
+export default Main
